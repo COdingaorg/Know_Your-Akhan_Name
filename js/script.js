d7b21e780fbaa9486397a926685deca418531adc
Caleb Odinga <caleb.odinga@student.moringaschool.com>
2021-04-11 14:22:13 +0300

    add function for calculating day of the week number

--- js/script.js
+++ js/script.js
@@ -5,3 +5,7 @@ let yearFull=prompt('Enter year of birth', 'YYYY');
   let year=parseInt((yearFull.charAt(2))+(yearFull.charAt(3)));
 let gender=(prompt('Enter Gender')).toLowerCase();
 
+function dayOfTheWeek(century, year, month, date){
+  return (( ( (century/4) -(2*century-1)) + ((5*(year/4)) ) + ((26*(month+1)/10)) + date )%7)
+  //( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) %7
+}
