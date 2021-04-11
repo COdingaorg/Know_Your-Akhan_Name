79ebb89e0778bcd68b0f2f9b8443b2f2dbcfcda3
Caleb Odinga <caleb.odinga@student.moringaschool.com>
2021-04-11 14:21:28 +0300

    addvariables for date, month, century, year and gender

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let date=parseInt(prompt('Enter date of birth', 'Day only (DD)'));
+let month=parseInt(prompt('Enter month of birth', 'MM'));
+let yearFull=prompt('Enter year of birth', 'YYYY');
+  let century=parseInt((yearFull.charAt(0))+(yearFull.charAt(1)));
+  let year=parseInt((yearFull.charAt(2))+(yearFull.charAt(3)));
+let gender=(prompt('Enter Gender')).toLowerCase();
+
