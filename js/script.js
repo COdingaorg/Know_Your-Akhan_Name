a84566534f000a427c218bbac0cec4b72fa4b08e
Caleb Odinga <caleb.odinga@student.moringaschool.com>
2021-04-11 14:40:33 +0300

    Add function to submit button, adds reset button and a reset function to it

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-let date=parseInt(prompt('Enter date of birth', 'Day only (DD)'));
+/*let date=parseInt(prompt('Enter date of birth', 'Day only (DD)'));
 let month=parseInt(prompt('Enter month of birth', 'MM'));
 let yearFull=prompt('Enter year of birth', 'YYYY');
   let century=parseInt((yearFull.charAt(0))+(yearFull.charAt(1)));
   let year=parseInt((yearFull.charAt(2))+(yearFull.charAt(3)));
-let gender=(prompt('Enter Gender')).toLowerCase();
-
+let gender=(prompt('Enter Gender')).toLowerCase();*/
+//function to calculate number that represents a day of the week
 function dayOfTheWeek(century, year, month, date){
   return (( ( (century/4) -(2*century-1)) + ((5*(year/4)) ) + ((26*(month+1)/10)) + date )%7)
   //( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) %7
+}
+//fuction for submitting
+function submitForm(){
+  document.getElementById('birthday').submit();
+}
+//function for reseting the form
+function resetForm(){
+  document.getElementById('birthday').reset();
 }
 let dayNumber=(dayOfTheWeek(century, year, month, date).toFixed());
 
