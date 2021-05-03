ef94f1f3343a3ec6ee8ad79c4c3a599a533dd2dc
Caleb Odinga <calemasanga@gmail.com>
2021-05-04 00:36:22 +0300

    adds correct gender input value

--- js/thirdScript.js
+++ js/thirdScript.js
@@ -3,36 +3,31 @@
 $(document).ready(function () {
   $('form#birthday').submit(function (event) {
     event.preventDefault();
-    $('form').css('background-color','green');
+    $('form').css('background-color', 'green');
     var userDateInput = $('input#day').val();
     var dateSetting = new Date(userDateInput);
     var weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     var bornDayNumber = dateSetting.getDay();
     var bornDay = weekDay[bornDayNumber];
-    console.log(bornDay)
+    
+    var gender = $('#gender[type="radio"]:checked').val();
     //Get Akan Names
-    let akanNamesMales=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-    let akanNamesFemales=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-      if (gender=='male') {
-        var userAkanName = akanNamesMales[validDayNumber]
-      }else if(gender=='female'){
-        var userAkanName = akanNamesFemales[validDayNumber]
-      }else if(gender=='other'){
-        $('#akanName p').before('<p>Sorry we dont have Name results for the selected option</p>');
-      }else if(gender==null||gender==''){
-        $('#akanName p').before('<p>You did not select any gender option</p>');
-      }else if(gender=='other'){
-        $('#akanName p').before('<p>Sorry we dont have Name results for the selected option</p>');
-      };
-      if(date==null||date==''){
-        $('#dayOfBirth p').before('<p>You did not enter any date</p>');
-      }
-      $('#dayOfBirth p').before('<p>'+bornDay+'</p>');
-      $('#akanName p').before('<p>'+userAkanName+'</p>');
-
-  event.preventDefault();
-  //deleting the first child
-  $('#dayOfBirth p:last-child').remove();
-  $('#akanName p:last-child').remove();
+    let akanNamesMales = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    let akanNamesFemales = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    if (gender == 'male') {
+      var userAkanName = akanNamesMales[bornDayNumber]
+    } else if (gender == 'female') {
+      var userAkanName = akanNamesFemales[bornDayNumber]
+    } else if (gender == 'other') {
+      $('#akanName p').before('<p>Sorry we dont have Name results for the selected option</p>');
+    } else if (gender == null || gender == ''||gender == undefined) {
+      $('#akanName p').before('<p>You did not select any gender option</p>');
+    };
+    //Printing day of borth and Akan Name
+    $('#dayOfBirth p').before('<p>' + bornDay + '</p>');
+    $('#akanName p').before('<p>' + userAkanName + '</p>');
+    //deleting the first child
+    $('#dayOfBirth p:last-child').remove();
+    $('#akanName p:last-child').remove();
   })
 })
